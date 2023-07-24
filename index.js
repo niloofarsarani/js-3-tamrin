7ab3f02c1bb8211b4a20c458074a52f0ca173fac
niloofarsarani <noloofarsarani8.gmail.com>
2023-07-24 09:25:17 +0330

    finish operator plus

---
+++ index.js
@@ -0,0 +1,19 @@
+const leftNumber = +prompt ("left number?");
+const operator= prompt ("operator?");
+const rightNumber = +prompt ("right number?");
+
+if (isNaN(leftNumber)){
+    consol.log("EER!")
+}
+
+if (isNaN( rightNumber)){
+    consol.log("EER!")
+}
+
+
+if(operator=== "+"){
+    consol.log("leftNumber + rightNumber")
+}
+
+
+
